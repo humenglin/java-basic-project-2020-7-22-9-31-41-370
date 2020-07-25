2e3df1d467ad6660a4383fc914d7ffcea733baf9
humenglin <humenglin@zyb.com.cn>
2020-07-25 17:53:56 +0800

    [feat]:将消费类型字符串转为消费类型枚举

--- src/main/java/com/thoughtworks/basic/credit/card/enumertion/PaymentPatternEnum.java
+++ src/main/java/com/thoughtworks/basic/credit/card/enumertion/PaymentPatternEnum.java
@@ -1,5 +1,21 @@
 package com.thoughtworks.basic.credit.card.enumertion;
 
 public enum PaymentPatternEnum {
-    POS_PAY;
+    POS_PAY("POS机消费");
+
+    private String desc;
+
+    PaymentPatternEnum(String desc) {
+        this.desc = desc;
+    }
+
+    public static PaymentPatternEnum form(String desc) {
+        for (PaymentPatternEnum paymentPattern : PaymentPatternEnum.values()) {
+            if (desc.equals(paymentPattern.desc)) {
+                return paymentPattern;
+            }
+        }
+
+        throw new IllegalArgumentException("未知的消费类型：" + desc);
+    }
 }
---
+++ src/test/java/com/thoughtworks/basic/credit/card/PaymentPatternEnumTest.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.basic.credit.card;
+
+import com.thoughtworks.basic.credit.card.enumertion.PaymentPatternEnum;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PaymentPatternEnumTest {
+    @Test
+    public void should_return_POS_PAY_given_POS机消费() {
+        String desc = "POS机消费";
+        PaymentPatternEnum paymentPatternEnum = PaymentPatternEnum.form(desc);
+        Assert.assertEquals(PaymentPatternEnum.POS_PAY, paymentPatternEnum);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_return_POS_PAY_given_other消费() {
+        String desc = "other消费";
+        PaymentPatternEnum.form(desc);
+    }
+}

38048f1977e7ed1d4b57f9d687828c70ef6c46d8
humenglin <humenglin@zyb.com.cn>
2020-07-25 18:24:38 +0800

    [feat]:处理多条消费记录转成对象

--- src/main/java/com/thoughtworks/basic/credit/card/CreditCardPointService.java
+++ src/main/java/com/thoughtworks/basic/credit/card/CreditCardPointService.java
@@ -6,22 +6,29 @@ import com.thoughtworks.basic.credit.card.enumertion.PaymentPatternEnum;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class CreditCardPointService {
+    public static final String ITEM_REGEX = "\n";
     public static final String REGEX = " ";
     public static final String CURRENCY_UNIT = "元";
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
-    public ConsumptionRecord transformToConsumptionRecords(String consumptionInfosStr) throws ParseException {
-        String[] consumptionInfos = consumptionInfosStr.split(REGEX);
+    public List<ConsumptionRecord> transformToConsumptionRecords(String consumptionInfosStr) throws ParseException {
+        List<ConsumptionRecord> consumptionRecords = new ArrayList<>();
+        String[] consumptionItems = consumptionInfosStr.split(ITEM_REGEX);
+        for (String consumptionItem: consumptionItems) {
+            String[] consumptionInfos = consumptionItem.split(REGEX);
 
-        String dateStr = consumptionInfos[0] + REGEX + consumptionInfos[1];
-        Date consumptionTime = simpleDateFormat.parse(dateStr);
-        PaymentPatternEnum paymentPattern = PaymentPatternEnum.form(consumptionInfos[2]);
-        BigDecimal amount = new BigDecimal(consumptionInfos[3].replace(CURRENCY_UNIT, ""));
+            String dateStr = consumptionInfos[0] + REGEX + consumptionInfos[1];
+            Date consumptionTime = simpleDateFormat.parse(dateStr);
+            PaymentPatternEnum paymentPattern = PaymentPatternEnum.form(consumptionInfos[2]);
+            BigDecimal amount = new BigDecimal(consumptionInfos[3].replace(CURRENCY_UNIT, ""));
 
-        ConsumptionRecord consumptionRecord = new ConsumptionRecord(consumptionTime, paymentPattern, CardTypeEnum.NORMAL_CARD, amount);
-        return consumptionRecord;
+            consumptionRecords.add(new ConsumptionRecord(consumptionTime, paymentPattern, CardTypeEnum.NORMAL_CARD, amount));
+        }
+        return consumptionRecords;
     }
 }
--- src/test/java/com/thoughtworks/basic/credit/card/CreditCardPointServiceTest.java
+++ src/test/java/com/thoughtworks/basic/credit/card/CreditCardPointServiceTest.java
@@ -8,7 +8,10 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class CreditCardPointServiceTest {
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
@@ -19,10 +22,32 @@ public class CreditCardPointServiceTest {
     public void should_return_ConsumptionRecord_when_transform_to_ConsumptionRecords_given_consumptionInfosStr() throws ParseException {
         String consumptionInfosStr = "2020-07-01 18:40 POS机消费 8元";
         Date consumptionTime = simpleDateFormat.parse("2020-07-01 18:40");
-        ConsumptionRecord expected = new ConsumptionRecord(consumptionTime, PaymentPatternEnum.POS_PAY, CardTypeEnum.NORMAL_CARD, new BigDecimal(8));
+        ConsumptionRecord consumptionRecord = new ConsumptionRecord(consumptionTime, PaymentPatternEnum.POS_PAY, CardTypeEnum.NORMAL_CARD, new BigDecimal(8));
+        List<ConsumptionRecord> expected = Arrays.asList(consumptionRecord);
 
-        ConsumptionRecord actual = service.transformToConsumptionRecords(consumptionInfosStr);
+        List<ConsumptionRecord> actual = service.transformToConsumptionRecords(consumptionInfosStr);
 
-        Assertions.assertThat(actual).isEqualToComparingFieldByField(expected);
+        Assertions.assertThat(actual.size()).isEqualTo(expected.size());
+        for (int i = 0; i < actual.size(); i++) {
+            Assertions.assertThat(actual.get(i)).isEqualToComparingFieldByField(expected.get(i));
+        }
+    }
+
+    @Test
+    public void should_return_ConsumptionRecords_when_transform_to_ConsumptionRecords_given_consumptionInfosStr() throws ParseException {
+        String consumptionInfosStr = "2020-07-01 18:40 POS机消费 8元\n" +
+                "2020-07-01 18:50 POS机消费 108元\n" +
+                "2020-07-02 18:50 POS机消费 208元";
+        ConsumptionRecord consumptionRecord1 = new ConsumptionRecord(simpleDateFormat.parse("2020-07-01 18:40"), PaymentPatternEnum.POS_PAY, CardTypeEnum.NORMAL_CARD, new BigDecimal(8));
+        ConsumptionRecord consumptionRecord2 = new ConsumptionRecord(simpleDateFormat.parse("2020-07-01 18:50"), PaymentPatternEnum.POS_PAY, CardTypeEnum.NORMAL_CARD, new BigDecimal(108));
+        ConsumptionRecord consumptionRecord3 = new ConsumptionRecord(simpleDateFormat.parse("2020-07-02 18:50"), PaymentPatternEnum.POS_PAY, CardTypeEnum.NORMAL_CARD, new BigDecimal(208));
+        List<ConsumptionRecord> expected = Arrays.asList(consumptionRecord1, consumptionRecord2, consumptionRecord3);
+
+        List<ConsumptionRecord> actual = service.transformToConsumptionRecords(consumptionInfosStr);
+
+        Assertions.assertThat(actual.size()).isEqualTo(expected.size());
+        for (int i = 0; i < actual.size(); i++) {
+            Assertions.assertThat(actual.get(i)).isEqualToComparingFieldByField(expected.get(i));
+        }
     }
 }
